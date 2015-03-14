66c766d56bb6c0aef7b0d312cb08acff6bc19234
cagegong <cagegong@163.com>
2015-03-14 23:37:03 +0800

    add music and more images

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,31 @@
 /* jshint devel:true */
 'use strict';
 $(document).ready(function() {
+  var reveal = function (selector) {
+    var img = $(selector);
+    if (!img[0].src&&img.attr('data-src')) {
+      img[0].src = img.attr('data-src');
+    }
+  };
+  var playing = true;
+  $('.music').on('click',function(){
+    var vid = document.getElementById('audio');
+    if (playing) {
+      playing = false;
+      vid.pause();
+      $(this).removeClass('rotate');
+    } else {
+      $(this).addClass('rotate');
+      playing = true;
+      vid.play();
+    }
+  });
   $('#fullpage').fullpage({
     controlArrows: false,
     loopHorizontal: false,
     afterSlideLoad: function(){
       var loadedSlide = $(this);
+      reveal(loadedSlide.find('img'));
       loadedSlide.removeClass('hide-img');
     },
     onSlideLeave: function(){
